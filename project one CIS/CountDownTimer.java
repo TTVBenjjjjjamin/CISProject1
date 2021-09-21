860fb1ca1b5495f112b071a180a06304592df562
Owencarroll5 <90726461+Owencarroll5@users.noreply.github.com>
2021-09-21 12:18:33 -0400

    Add files via upload

---
+++ project one CIS/CountDownTimer.java
@@ -0,0 +1,12 @@
+package Project1GIVE_TO_STUDENTS;
+
+public class CountDownTimer {
+	
+	public CountDownTimer() {
+		
+	}
+
+	public CountDownTimer(int hours, int minutes, int seconds) {
+		
+	}
+}
